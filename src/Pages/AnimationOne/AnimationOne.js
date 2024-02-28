740209f8d5c13490b3b774a6a2f966d2a7cf7cf3
adamshoaib <akareems@asu.edu>
2024-02-27 21:11:28 -0700

    added road, grass, vehical, added animations

--- src/Pages/AnimationOne/AnimationOne.js
+++ src/Pages/AnimationOne/AnimationOne.js
@@ -1,13 +1,42 @@
 import react from "react";
 import "./AnimationOne.scss";
 import Cloud from "../../Assets/cloud.svg";
+import Car from "../../Assets/car.svg";
+import Ambulance from "../../Assets/ambulance.svg";
 
 const AnimationOne = () => {
   return (
     <>
-      <div>
-        <img width="400" height="300" src={Cloud} alt="cloud" />
+      <div className="animation-one-container">
+        <img
+          className="cloud-img"
+          width="300"
+          height="250"
+          src={Cloud}
+          alt="cloud"
+        />
       </div>
+      <div className="animation-one-grass"></div>
+      <div className="animation-one-road">
+        <div className="animation-one-road-lines"></div>
+        <img
+          className="car-img"
+          width="200"
+          height="250"
+          src={Car}
+          alt="cloud"
+        />
+        <img
+          className="ambulance-img"
+          width="200"
+          height="190"
+          src={Ambulance}
+          alt="cloud"
+        />
+      </div>
+      {/* <div className="animation-one-button-container">
+        <button className="hover-button">Hover Me</button>
+      </div> */}
     </>
   );
 };
