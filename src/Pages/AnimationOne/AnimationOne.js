7a19f1a7f92fb314e15961cf3ee49c18f02f973c
adamshoaib <akareems@asu.edu>
2024-02-27 17:55:44 -0700

    added project structure, animation one folder, cloud logo

---
+++ src/Pages/AnimationOne/AnimationOne.js
@@ -0,0 +1,15 @@
+import react from "react";
+import "./AnimationOne.scss";
+import Cloud from "../../Assets/cloud.svg";
+
+const AnimationOne = () => {
+  return (
+    <>
+      <div>
+        <img width="400" height="300" src={Cloud} alt="cloud" />
+      </div>
+    </>
+  );
+};
+
+export default AnimationOne;
